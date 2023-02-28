56536aceabd0c43d93ea5b2de0dffad1151f8357
Trevor Stockdale <stockdale92@gmail.com>
2023-02-28 12:56:11 +0100

    added models, public and routes

---
+++ routes/campgrounds.js
@@ -0,0 +1,46 @@
+const express = require("express");
+const router = express.Router();
+const catchAsync = require("../utilities/catchAsync");
+const campgrounds = require("../controllers/campgrounds");
+const { isLoggedIn, isAuthor, validateCampground } = require("../middleware");
+const multer = require("multer");
+const { storage } = require("../cloudinary");
+const upload = multer({ storage });
+
+///////////////////////////
+//// Router movement Get put post delete....////
+/////////////////
+
+router
+  .route("/")
+  .get(catchAsync(campgrounds.index))
+  .post(
+    isLoggedIn,
+    upload.array("image"),
+    validateCampground,
+    catchAsync(campgrounds.createCampgrounds)
+  );
+
+router.get("/new", isLoggedIn, campgrounds.createNewForm);
+
+router
+  .route("/:id")
+  .get(catchAsync(campgrounds.showCampgrounds))
+  .put(
+    isLoggedIn,
+    isAuthor,
+    validateCampground,
+    upload.array("image"),
+
+    catchAsync(campgrounds.updateCampgrounds)
+  )
+  .delete(isLoggedIn, isAuthor, catchAsync(campgrounds.deleteCampgrounds));
+
+router.get(
+  "/:id/edit",
+  isLoggedIn,
+  isAuthor,
+  catchAsync(campgrounds.editCampgrounds)
+);
+
+module.exports = router;
